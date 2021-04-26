42bdf8d99f6170db96a5f9e5eaa0c06fc508aa7c
Ermal Milazimi <milazimi.ermal@gmail.com>
2021-04-26 18:48:19 +0200

    header with video ,footer added, content with js to change words

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+const spanTxt = document.querySelector('.exploreTxtSpan')
+
+const spanTxtData = ["Restaurants","Nature"]
+let idx = 0;
+
+function changeSpan(){
+  spanTxt.innerHTML = spanTxtData[idx]
+  idx++
+  if(idx === spanTxtData.length) idx = 0;
+}
+
+setInterval(changeSpan, 3000)
